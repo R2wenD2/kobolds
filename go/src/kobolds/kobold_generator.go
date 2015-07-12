80410be34e2f4fcc4a3eb47cca90bbfdc9032d05
Wendy Dembowski <whillegass@gmail.com>
2015-07-12 09:24:47 -0400

    Update skills to actually generate skills rather than using a constant

--- go/src/kobolds/kobold_generator.go
+++ go/src/kobolds/kobold_generator.go
@@ -54,10 +54,102 @@ func generateKobold() Kobold {
 	luck := calculateHandyNumber(extraneous)
 	reflexes := rollDice(2)
 	agility := calculateHandyNumber(reflexes)
-	return Kobold{"klak", brawn, ego, extraneous, reflexes, brawn,
-		meat, cunning, luck, agility, p, p, p, armor, gear, weapon}
+	skills := chooseSkills(ego)
+	hits := brawn // This also needs to include modifiers from gear
+	return Kobold{"klak", brawn, ego, extraneous, reflexes, hits,
+		meat, cunning, luck, agility, skills, p, p, armor, gear, weapon}
 }
 
+func chooseSkills(ego int) []string {
+	numskills := 6
+	if ego < 6 {
+		numskills = ego
+	}
+	skills := make([]string, numskills)
+
+	for i := range skills {
+		skills[i] = getSkill(i)
+
+	}
+
+	// Need to generate at least one of each skill type, and then Random
+	// for coming back to this.
+
+	return skills
+}
+
+func getSkill(i int) string {
+	switch i {
+	case 0:
+		return "Cook"
+	case 1:
+		return getBrawnSkill()
+	case 2:
+		return getEgoSkill()
+	case 3:
+		return getExtraneousSkill()
+	case 4:
+		return getReflexesSkill()
+	default:
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		return getSkill(r.Intn(3) + 1)
+	}
+
+}
+
+func getBrawnSkill() string {
+	brawnSkillMap := map[int]string{
+		1: "Athlete",
+		2: "Bully",
+		3: "Duel",
+		4: "Lift",
+		5: "Swim",
+		6: "Wrassle",
+	}
+	return brawnSkillMap[rollDice(1)]
+}
+
+func getEgoSkill() string {
+	skillMap := map[int]string{
+		1: "Hide",
+		2: "Lackey",
+		3: "Sage",
+		4: "Shoot",
+		5: "Speak Human",
+		6: "Trap",
+	}
+	return skillMap[rollDice(1)]
+}
+
+func getExtraneousSkill() string {
+	skillMap := map[int]string{
+		1:  "Bard",
+		2:  "Dungeon",
+		3:  "Track",
+		4:  "Trade",
+		5:  "Bard",
+		6:  "Dungeon",
+		7:  "Track",
+		8:  "Trade",
+		9:  "Bard",
+		10: "Dungeon",
+		11: "Track",
+		12: "Trade",
+	}
+	return skillMap[rollDice(3)]
+}
+
+func getReflexesSkill() string {
+	skillMap := map[int]string{
+		1: "Cower",
+		2: "Fast",
+		3: "Sneak",
+		4: "Steal",
+		5: "Ride",
+		6: "Wiggle",
+	}
+	return skillMap[rollDice(1)]
+}
 func getGear() Gear {
 	// TODO(whillegas): make this a constant so we don't invoke it each time.
 	gearMap := map[int]Gear{
@@ -94,12 +186,12 @@ func init() {
 	http.HandleFunc("/", handler)
 }
 
-func rollDice(num_dice int) int {
+func rollDice(numDice int) int {
 	result := 0
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < num_dice; i++ {
+	for i := 0; i < numDice; i++ {
 
-		result += r.Intn(7)
+		result += r.Intn(6) + 1
 	}
 	return result
 }
